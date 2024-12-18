1912b2291fddfa6120927292e124da3bb7d9926d
GANTA SAI KIRAN <163864060+SaiKiranGanta2003@users.noreply.github.com>
2024-12-18 09:42:10 +0530

    Update document.js

--- models/document.js
+++ models/document.js
@@ -51,27 +51,10 @@ const documentSchema = new mongoose.Schema({
     }],
   }, {
   timestamps: true,
-  
 });
-
 // Ensure no OverwriteModelError by checking if model already exists
 const modelName = 'DocumentData'; // Replace with your model 
-
-<<<<<<< HEAD
 const Document = mongoose.models[modelName] || mongoose.model(modelName, documentSchema);
 export default Document;
-=======
 module.exports = Document;
 
-
-
-// const documentSchema = new mongoose.Schema({
-//     title: { type: String, required: true },
-//     reviewers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-//     approver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//     status: { type: String, required: true, enum: ['Pending Review', 'Approved', 'Rejected'] },
-//     comments: [{ type: String }],
-// });
-
-// module.exports = mongoose.model('Document', documentSchema);
->>>>>>> 7ac40b5e587579e770463fe95096731e517bb78d
